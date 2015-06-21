fdb029a1308974bd0108bd4c742b9d0ce3077596
Alfonso Gómez-Arzola <alfonso@mrkp.co>
2015-06-20 23:04:18 -0400

    Select tab according to url for play/pause toggle

--- scripts/background.js
+++ scripts/background.js
@@ -1,5 +1,5 @@
 chrome.commands.onCommand.addListener(function (command) {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  chrome.tabs.query({ url: '*://play.pocketcasts.com/*' }, function (tabs) {
     chrome.tabs.sendMessage(tabs[0].id, { command: command })
   })
 })
