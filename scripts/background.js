0019a13049e0d0011b4dae8b5a06a6328548d487
Alfonso Gómez-Arzola <alfonso@mrkp.co>
2015-06-20 22:57:23 -0400

    Add play/pause toggle support when window & tab are frontmost

---
+++ scripts/background.js
@@ -0,0 +1,5 @@
+chrome.commands.onCommand.addListener(function (command) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { command: command })
+  })
+})
--- scripts/voodoo.js
+++ scripts/voodoo.js
@@ -3,3 +3,16 @@ var styleText = document.createTextNode('.episode_button.ng-hide,.episode_time.n
 styleTag.type = 'text/css'
 styleTag.appendChild(styleText)
 document.body.appendChild(styleTag)
+
+var playPauseToggle = function () {
+  var audioPlayerWrapper = document.getElementById('audio_player_wrapper')
+  var playPauseButton = audioPlayerWrapper.getElementsByClassName('play_pause_button')[0]
+  var clickEvent = new MouseEvent('click')
+  playPauseButton.dispatchEvent(clickEvent)
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.command == 'play_pause_toggle') {
+    playPauseToggle()
+  }
+})
