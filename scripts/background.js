6cbeb4cc3051303752d067a0c10fe83cf79abf61
Alfonso Gómez-Arzola <alfonso@mrkp.co>
2015-06-22 11:36:10 -0400

    Comment code by section/functionality

--- scripts/background.js
+++ scripts/background.js
@@ -1,5 +1,7 @@
+// Listen for play/pause media key presses
 chrome.commands.onCommand.addListener(function (command) {
   chrome.tabs.query({ url: '*://play.pocketcasts.com/*' }, function (tabs) {
+    // Send message to contentscript
     chrome.tabs.sendMessage(tabs[0].id, { command: command })
   })
 })
--- scripts/voodoo.js
+++ scripts/voodoo.js
@@ -1,9 +1,11 @@
+// Inject styles overriding dislpay:hidden
 var styleTag = document.createElement('style')
 var styleText = document.createTextNode('.episode_button.ng-hide,.episode_time.ng-hide{display:block !important;}')
 styleTag.type = 'text/css'
 styleTag.appendChild(styleText)
 document.body.appendChild(styleTag)
 
+// Play/pause toggle behavior
 var playPauseToggle = function () {
   var audioPlayerWrapper = document.getElementById('audio_player_wrapper')
   var playPauseButton = audioPlayerWrapper.getElementsByClassName('play_pause_button')[0]
@@ -11,6 +13,7 @@ var playPauseToggle = function () {
   playPauseButton.dispatchEvent(clickEvent)
 }
 
+// Listen for messages from document.js
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.command == 'play_pause_toggle') {
     playPauseToggle()
